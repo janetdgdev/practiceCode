9b24a4059fe2148ce77e07940292a5d41d3538fd
Janet Diaz <janetdg.dev@gmail.com>
2024-12-27 17:35:58 -0800

    Add files via upload

---
+++ leetcode/1672-richest-customer.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+    let richest = 0
+    //Loop through the account users
+    for( let i = 0; i <accounts.length; i++){
+        //Reduce all accounts to find the highest number
+        const sum = accounts[i].reduce((i,j) => {return i+j }, 0)
+        if(sum > richest) {
+        richest = sum;
+        }
+    }
+    return richest
+};
